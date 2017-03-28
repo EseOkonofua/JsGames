8129bf6d6def2592dd1894f6344a20095cf2cc2e
Ese Okonofua <eseokonofua95@gmail.com>
2017-03-28 15:12:20 -0400

    Hey first push

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ var app = express();
 var port = process.env.PORT || 3000;
 app.set('port',port);
 
+app.use(express.static('lib'));
 app.use('/', express.static('./'));
 app.get('/',function(req,res){
+  console.log("In / path")
   res.sendFile('./index');
 });
 
 app.use('/raindrops', express.static('./RainDrops'));
 app.get("/raindrops",function(req,res){
+  res.sendFile('./index');
+});
+
+app.use('/spacehipster', express.static('./SpaceHipster'));
+app.get("/spacehipster",function(req,res){
   res.sendFile('./index');
 });
 
